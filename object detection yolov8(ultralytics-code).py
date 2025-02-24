495e1d11e7d7c1d1f6227a04ac6d5974765f4153
mohdmuneebuddin <muneebuddin136@gmail.com>
2025-02-24 12:39:20 +0530

    Add files via upload

---
+++ object detection yolov8(ultralytics-code).py
@@ -0,0 +1,29 @@
+import cv2
+import torch
+from ultralytics import YOLO
+
+# Load the YOLOv8 model
+model = YOLO("yolov8n.pt")
+
+# Initialize webcam (default camera)
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Perform YOLO inference
+    results = model(frame)
+
+    # Plot bounding boxes on the frame
+    bounding_boxes = results[0].plot()
+
+    # Display the frame
+    cv2.imshow("YOLOv8 Default camera Object Detection", bounding_boxes)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):  # Press 'q' to exit
+        break
+
+cap.release()
+cv2.destroyAllWindows()
